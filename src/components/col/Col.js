a5d25b4672f102ad21a2d203ed9c4421f0f27aa4
dstephany <dstephany@nextgen.com>
2019-07-15 14:08:18 -0700

    added propTypes to Col

--- src/components/col/Col.js
+++ src/components/col/Col.js
@@ -1,27 +1,60 @@
 import React from 'react';
 import './col.scss';
 import applyClassName from '../_utils/applyClassName/applyClassName.js';
+import PropTypes from 'prop-types';
 
-export default React.forwardRef(function flexCol(props, ref) {
-    const { className, children, xs, sm, md, lg, xl, offsetXs,
-         offsetSm, offsetMd, offsetLg, offsetXl, ...others} = props,
+const Col = React.forwardRef(function col(props, ref) {
+    const { 
+        className,
+        children, 
+        xs, 
+        sm, 
+        md, 
+        lg, 
+        xl, 
+        offsetXs, 
+        offsetSm, 
+        offsetMd, 
+        offsetLg, 
+        offsetXl, 
+        ...others
+    } = props,
         sizes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
-    let classList = "";
+    let classes = "";
 
-    classList = applyClassName(classList, 'rbui-col-', xs, sizes);
-    classList = applyClassName(classList, 'rbui-col-sm-', sm, sizes);
-    classList = applyClassName(classList, 'rbui-col-md-', md, sizes);
-    classList = applyClassName(classList, 'rbui-col-lg-', lg, sizes);
-    classList = applyClassName(classList, 'rbui-col-xl-', xl, sizes);
-    classList = applyClassName(classList, 'rbui-offset-col-', offsetXs, sizes);
-    classList = applyClassName(classList, 'rbui-offset-col-sm-', offsetSm, sizes);
-    classList = applyClassName(classList, 'rbui-offset-col-md-', offsetMd, sizes);
-    classList = applyClassName(classList, 'rbui-offset-col-lg-', offsetLg, sizes);
-    classList = applyClassName(classList, 'rbui-offset-col-xl-', offsetXl, sizes);
+console.log(xl);
 
-    classList = classList || "rbui-col";
+    classes = applyClassName(classes, 'rbui-col-', xs, sizes);
+    classes = applyClassName(classes, 'rbui-col-sm-', sm, sizes);
+    classes = applyClassName(classes, 'rbui-col-md-', md, sizes);
+    classes = applyClassName(classes, 'rbui-col-lg-', lg, sizes);
+    classes = applyClassName(classes, 'rbui-col-xl-', xl, sizes);
+    classes = applyClassName(classes, 'rbui-offset-col-', offsetXs, sizes);
+    classes = applyClassName(classes, 'rbui-offset-col-sm-', offsetSm, sizes);
+    classes = applyClassName(classes, 'rbui-offset-col-md-', offsetMd, sizes);
+    classes = applyClassName(classes, 'rbui-offset-col-lg-', offsetLg, sizes);
+    classes = applyClassName(classes, 'rbui-offset-col-xl-', offsetXl, sizes);
 
-    classList = className ? `${className} ${classList}` : classList;
+    classes = classes || "rbui-col";
 
-    return <div className={classList} ref={ref} {...others} >{children}</div>
-})
+    classes = className ? `${className} ${classes}` : classes;
+
+    return <div className={classes} ref={ref} {...others} >{children}</div>
+});
+
+Col.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.any, 
+    xs: PropTypes.string, 
+    sm: PropTypes.string, 
+    md: PropTypes.string, 
+    lg: PropTypes.string, 
+    xl: PropTypes.string, 
+    offsetXs: PropTypes.string, 
+    offsetSm: PropTypes.string, 
+    offsetMd: PropTypes.string, 
+    offsetLg: PropTypes.string, 
+    offsetXl: PropTypes.string, 
+}
+
+export default Col;
