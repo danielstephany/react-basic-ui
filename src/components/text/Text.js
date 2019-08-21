73e46c695b9e745604a2e9c8d34f9ed80bd276fe
dstephany <dstephany@nextgen.com>
2019-08-21 13:43:05 -0700

    added text component

--- src/components/text/Text.js
+++ src/components/text/Text.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Text = React.forwardRef((props, ref)=> {
     const {className, children, varient, mimic, lg, ...others} = props;
     let element;
-    let classes = className ? 'rbui-text ' + className : 'rbui-text';
+    let classes = className ? `rbui-text-${varient} ${className}` : 'rbui-text-' + varient;
     let mimicOptions = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'body']
 
 
@@ -25,6 +25,8 @@ const Text = React.forwardRef((props, ref)=> {
         element = <h6 className={classes} {...others} ref={ref}>{children}</h6>
     } else if(varient === "body") {
         element = <p className={classes} {...others} ref={ref}>{children}</p>
+    } else {
+        element = <span className={'rbui-text ' + className} {...others} ref={ref}>{children}</span>
     }
 
     return element;
