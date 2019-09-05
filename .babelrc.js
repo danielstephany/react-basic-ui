e629bf0378204c9ac8dd1d12f1e62e0717bbd94f
dstephany <dstephany@nextgen.com>
2019-09-05 12:08:38 -0700

    added jest and got most tests to pass

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+    plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-class-properties', '@babel/plugin-transform-react-jsx']
+  };
---
+++ jest/config.js
@@ -0,0 +1,12 @@
+ module.exports = {
+    "verbose": true,
+    "rootDir": "../",
+    "roots": ["<rootDir>/src"],
+    "setupFilesAfterEnv": ["<rootDir>/jest/setupTests.js"],
+    "moduleFileExtensions": ["js", "jsx"],
+    "moduleDirectories": ["node_modules"],
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/mocks/fileMock.js",
+        "\\.(css|less|scss)$": "<rootDir>/jest/mocks/styleMock.js"
+      }
+}
