c67de2a7287d8c2c4e054e4b9c7a2d7011cb28ab
dstephany <dstephany@nextgen.com>
2019-09-12 09:13:38 -0700

    added tests to header

--- src/components/appHeader/appHeader.test.js
+++ src/components/appHeader/appHeader.test.js
@@ -7,9 +7,29 @@ describe('AppHeader test suit', () => {
         const wrap = shallow(<AppHeader></AppHeader>);
     });
 
+    it('it renders with proper added classes', () => {
+        const wrap = mount(<AppHeader className="testClass" fixed="top"></AppHeader>);
+        expect(wrap.find('header').hasClass('rbui-app-header testClass')).toBe(true);
+    });
+
     it('it renders with proper classes for fixed to top', () => {
         const wrap = mount(<AppHeader fixed="top"></AppHeader>);
         expect(wrap.find('header').hasClass('rbui-app-header rbui-app-header--fixed-top')).toBe(true);
     });
-});
 
+    it('it renders with proper classes for fixed to bottom', () => {
+        const wrap = mount(<AppHeader fixed="bottom"></AppHeader>);
+        expect(wrap.find('header').hasClass('rbui-app-header rbui-app-header--fixed-bottom')).toBe(true);
+    });
+
+    it('renders without the spacer element when it is not passed the fixed spacer prop', () => {
+        const wrap = mount(<AppHeader fixed="bottom" ></AppHeader>);
+        expect(wrap.find('.rbui-app-header__spacer').exists()).toBe(false);
+    });
+
+    it('renders with the spacer element when passed the fixed spacer prop', () => {
+        const wrap = mount(<AppHeader fixed="bottom" fixedSpacer></AppHeader>);
+        expect(wrap.find('.rbui-app-header__spacer').exists()).toBe(true);
+    });
+
+});
