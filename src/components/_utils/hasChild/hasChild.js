9da788af516963f3903a7cd312338b85fd72d7c0
danielstephany <danielstephany85@gmail.com>
2019-09-16 19:18:51 -0700

    Added tests to listItem

--- src/components/_utils/hasChild/hasChild.js
+++ src/components/_utils/hasChild/hasChild.js
@@ -4,7 +4,6 @@ export default function hasChild (children, childName) {
 
     const childrenArray = React.Children.toArray(children);
     let hasCustomName = false;
-    console.log(childrenArray);
     childrenArray.forEach(item => {
         if (item.type && item.type.customNameRef === childName) {
             hasCustomName = true;
--- src/components/listItem/listItem.test.js
+++ src/components/listItem/listItem.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ListItem from './listItem.js';
-import { shallow } from 'enzyme';
+import ListItemSecondary from '../listItemSecondary/listItemSecondary.js';
+import { shallow, mount } from 'enzyme';
 
 describe('ListItem unit tests', function(){
     it('mounts without crashing', function(){
         shallow(<ListItem />);
     });
 
-    it('mounts with proper classes', function(){
+    it('mounts with added classes', function(){
         const mount = shallow(<ListItem className="test" />);
         expect(mount.hasClass('list-item test')).toBe(true);
     });
@@ -16,4 +17,21 @@ describe('ListItem unit tests', function(){
         const mount = shallow(<ListItem>test</ListItem>);
         expect(mount.text()).toBe('test');
     })
+
+    it('mounts with the right classes when passed hasAction', () => {
+        const wrap = shallow(<ListItem hasAction></ListItem>);
+        expect(wrap.hasClass("list-item--action")).toBe(true);
+    });
+
+    it('mounts without the "list-item--with-secondary class', () => {
+        const wrap = shallow(<ListItem></ListItem>);
+        expect(wrap.hasClass('list-item--with-secondary')).toBe(false);
+    });
+});
+
+describe('listItem integration test suit', () => {
+    it('mounts with the "list-item--with-secondary', () => {
+        const wrap = shallow(<ListItem><ListItemSecondary>test</ListItemSecondary></ListItem>);
+        expect(wrap.hasClass('list-item--with-secondary')).toBe(true);
+    });
 });
