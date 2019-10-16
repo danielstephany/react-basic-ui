575d9a1bff464299744eb58a5fc0d829cf6a8681
danielstephany <danielstephany85@gmail.com>
2019-10-15 21:34:53 -0700

    finished BaseLabel test

--- src/components/baseLabel/BaseLabel.js
+++ src/components/baseLabel/BaseLabel.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './baseLabel.less';
 
 const BaseLabel = React.forwardRef(function baseLabel(props, rel){
-    const {className, children, ...others} = props;
-    const classes = className ? 'rbui-label ' + className : 'rbui-label';
+    const {className, children, inline, ...others} = props;
+    let classes = className ? 'rbui-label ' + className : 'rbui-label';
+
+    if (inline) {
+        classes += ' rbui-label--inline';
+    }
 
     return <label className={classes} {...others}>{children}</label>
 });
--- src/components/baseLabel/baseLabel.test.js
+++ src/components/baseLabel/baseLabel.test.js
@@ -11,4 +11,9 @@ describe('BaseLabel test suite', function(){
         const wrap = shallow(<BaseLabel>my label</BaseLabel>);
         expect(wrap.text()).toBe("my label");
     });
+
+    it('renders with proper classes when passed "inline"', function() {
+        const wrap = shallow(<BaseLabel inline></BaseLabel>);
+        expect(wrap.hasClass('rbui-label--inline')).toBe(true);
+    })
 });
