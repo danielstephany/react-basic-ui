5af3b680ca02f01db0d5c94ce5179b78b3ccb548
danielstephany <danielstephany85@gmail.com>
2019-10-25 15:26:02 -0700

    added fieldset component

--- docs/views/gridView/InteractiveGridSpace.js
+++ docs/views/gridView/InteractiveGridSpace.js
@@ -2,13 +2,13 @@ import React, {useState} from 'react';
 import Grid from 'components/grid/Grid.js';
 import Paper from  'components/paper/Paper.js';
 import Form from 'components/form/Form.js';
+import FieldSet from 'components/fieldSet/FieldSet.js';
 import BaseRadio from 'components/baseRadio/BaseRadio.js';
 
-const InteractiveGridSpace = () => {
-    const [spacingInput, setSpacingInput] = useState('8');
-    const [spacing, setSpacing] = useState(spacing);
+const InteractiveGridSpace = (props) => {
+    const [spacing, setSpacing] = useState('');
 
-    function setit(e){
+    function setSpacingValue(e){
         setSpacing(e.target.value);
     }
 
@@ -31,20 +31,21 @@ const InteractiveGridSpace = () => {
                     <Grid row>
                         <Grid col xs="12">
                             <Paper className="textc">
-                                <Form onSubmit={setit}>
-                                    {/* <input type="number" min="0" max="10" value={spacingInput} onChange={(e)=> {setSpacingInput(e.target.value)}}/> */}
-                                    <BaseRadio onChange={setit} value="0" label="0" name="test" />
-                                    <BaseRadio onChange={setit} value="1" label="1" name="test" />
-                                    <BaseRadio onChange={setit} value="2" label="2" name="test" />
-                                    <BaseRadio onChange={setit} value="3" label="3" name="test" />
-                                    <BaseRadio onChange={setit} value="4" label="4" name="test" />
-                                    <BaseRadio onChange={setit} value="5" label="5" name="test" />
-                                    <BaseRadio onChange={setit} value="6" label="6" name="test" />
-                                    <BaseRadio onChange={setit} value="7" label="7" name="test" />
-                                    <BaseRadio onChange={setit} value="8" label="8" name="test" />
-                                    <BaseRadio onChange={setit} value="9" label="9" name="test" />
-                                    <BaseRadio onChange={setit} value="10" label="10" name="test" />
-                                    <BaseRadio onChange={setit} value="" label="Default" name="test" labelLeft/>
+                                <Form onSubmit={setSpacingValue}>
+                                    <FieldSet legend="Modify column padding" fullWidth>
+                                        <BaseRadio onChange={setSpacingValue} value="0" label="0" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="1" label="1" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="2" label="2" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="3" label="3" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="4" label="4" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="5" label="5" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="6" label="6" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="7" label="7" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="8" label="8" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="9" label="9" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="10" label="10" name="test" />
+                                        <BaseRadio onChange={setSpacingValue} value="" label="Default" name="test" labelLeft/>
+                                    </FieldSet>                                    
                                 </Form>
                             </Paper>
                         </Grid>
